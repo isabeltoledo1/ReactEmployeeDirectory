cf0dfd8064bb4beb7be359552b458e11f8fc7447
Isabel Toledo <isabeltoledo9@gmail.com>
2021-04-28 20:38:32 -0600

    added searchbar

---
+++ src/components/search/index.js
@@ -0,0 +1,28 @@
+import React, {useContext} from "react";
+
+
+
+const SearchBar = () => {
+    const context = useContext(UserContext);
+
+    return (
+        <div className="searchbox">
+      <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="">
+              Search
+            </span>
+          </div>
+          <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="name"
+          aria-label="Search"
+          onChange={e => context.handleSearch(e)}
+        />
+        </div>
+    </div>
+    )
+}
+
+export default SearchBar;
