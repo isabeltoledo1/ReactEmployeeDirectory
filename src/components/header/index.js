e7d8fc4b50be973ffcb064dc2a2c9034e68a99c8
Isabel Toledo <isabeltoledo9@gmail.com>
2021-04-28 20:32:54 -0600

    added wrapper and header

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Header () {
+    return (
+      <div className="header">
+        <h1>Employee Directory</h1>
+        <p>Click heading to filter by heading or use the search box to narrow your results.</p>
+      </div>
+    )
+  }
+
+export default Header;
